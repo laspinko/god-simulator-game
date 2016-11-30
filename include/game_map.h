d1326f1e00c59acd2a15b5f8a0728f76eac4c69c
unknown <209-13@KAB209-13.smg.local>
2016-11-30 15:46:26 +0200

    Map coloring

--- include/game_map.h
+++ include/game_map.h
@@ -1,14 +1,27 @@
 #include<SDL2/SDL.h>
+#include<vector>
 
 #ifndef GAME_MAP_H
 #define GAME_MAP_H
 
+struct color{
+    int r, g, b;
+    color(int red = 0, int green = 0, int blue = 0):r(red), g(green), b(blue){};
+};
+
+struct color_gradient {
+    std::vector<double> p;
+    std::vector<color> c;
+    void push_color(double a, color b);
+    color get_color(double a);
+
+};
 
 struct map_tile {
     double height;
     double water;
 
-    void draw(SDL_Renderer* r, int x, int y);
+    void draw(SDL_Renderer* r, int x, int y, color_gradient& cr);
 };
 
 class game_map {
@@ -18,9 +31,10 @@ class game_map {
         void draw(SDL_Renderer* r);
         game_map& operator=(const game_map& other);
 
-        map_tile** m;
         int width, height;
     protected:
+        map_tile** m;
+        color_gradient gr;
 
     private:
 };
--- src/game_map.cpp
+++ src/game_map.cpp
@@ -2,13 +2,27 @@
 #include "perlin_noise.h"
 #include<iostream>
 
-void map_tile::draw(SDL_Renderer* r, int x, int y) {
-    if(water > height)  SDL_SetRenderDrawColor(r, 0, 0, 255, 255); // blue
-    else{
-        int n = height * 255.0;
-        SDL_SetRenderDrawColor(r, n, n / 2 , 0, 255);
-        //SDL_SetRenderDrawColor(r, 102, 51, 0, 255); // brown
+void color_gradient::push_color(double a, color b) {
+    p.push_back(a);
+    c.push_back(b); //TODO insertion
+}
+color color_gradient::get_color(double a) {
+    for(int i = 0; i < p.size() - 1; i ++) {
+        if(p[i] <= a && a < p[i + 1]) {
+            color ans;
+            double p2 = (a - p[i]) / (p[i + 1] - p[i]);
+            double p1 = (p[i + 1] - a) / (p[i + 1] - p[i]);
+            ans.r = p1 * c[i].r + p2 * c[i + 1].r;
+            ans.g = p1 * c[i].g + p2 * c[i + 1].g;
+            ans.b = p1 * c[i].b + p2 * c[i + 1].b;
+            return ans;
+        }
     }
+}
+
+void map_tile::draw(SDL_Renderer* r, int x, int y, color_gradient& gr) {
+    color c = gr.get_color(height);
+    SDL_SetRenderDrawColor(r, c.r, c.g, c.b, 255);
     SDL_RenderDrawPoint(r, x, y);
 }
 
@@ -31,7 +45,20 @@ game_map::game_map(int x, int y, int seedh, int seedw) {
             mx = std::max(m[i][j].height, mx);
         }
     }
-    std::cout << mn << " " << mx << std::endl;
+    for(int i = 0; i < x; i ++) {
+        for(int j = 0; j < y; j ++) {
+            m[i][j].height = (m[i][j].height - mn) / (mx - mn);
+            m[i][j].water = (m[i][j].water - mn) / (mx - mn) / 2;
+        }
+    }
+
+    //COLOR GRADIENTS
+    gr.push_color(0, color(0, 0, 0)); //black
+    gr.push_color(0.2, color(0, 0, 255)); //black
+    gr.push_color(0.15, color(255, 255, 0)); //black
+    gr.push_color(0.3, color(0, 191, 0)); //green
+    gr.push_color(0.7, color(196, 169, 110)); //brownish
+    gr.push_color(1, color(255,255,255)); //black
 }
 
 game_map& game_map::operator=(const game_map& other) {
@@ -59,7 +86,7 @@ game_map::~game_map() {
 void game_map::draw(SDL_Renderer* r) {
     for(int x = 0; x < width; x ++) {
         for(int y = 0; y < height; y ++) {
-            m[x][y].draw(r, x, y);
+            m[x][y].draw(r, x, y, gr);
         }
     }
 }
